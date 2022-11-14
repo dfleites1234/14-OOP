e5613fabc4cf205baf5747b3b3e661dd088f5f11
Sparrow <dfleites1234@gmail.com>
2022-11-14 12:13:12 -0600

    Setters and Getters

--- starter/script.js
+++ starter/script.js
@@ -126,8 +126,8 @@ bmw.accelerate();
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -137,13 +137,29 @@ class PersonCl {
   }
 
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -155,3 +171,25 @@ jessica.greet();
 // 1. Classes are NOT hoisted. Hoisted means we can use them before they are declared in the code.
 // 2. Classes are first-class citizens. Which means we can pass them into functions and also turn them from functions
 // 3. Classes are executed in strict mode.
+
+// Setters and Getters
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
